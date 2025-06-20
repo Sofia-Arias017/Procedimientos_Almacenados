a4e1e3c9a48bc5badc8e4780aa6ba09eab157aaa
Sofia-Arias017 <189870116+Sofia-Arias017@users.noreply.github.com>
2025-06-20 11:29:21 -0500

    feat: :sparkles: Ejercicio 2 funciones Descuento por Cantidad

--- Funciones.sql
+++ Funciones.sql
@@ -33,4 +33,34 @@ DELIMITER ;
 
 SELECT fc_calcular_subtotal_pizza(1) AS subtotal;
 
+--2. `fc_descuento_por_cantidad`
+--Parámetros: `p_cantidad INT`, `p_precio_unitario DECIMAL`
+--Si `p_cantidad ≥ 5` aplica 10% de descuento, sino 0%. Retorna el monto de descuento.
+
+DELIMITER $$
+
+DROP FUNCTION IF EXISTS fc_descuento_por_cantidad $$
+
+CREATE FUNCTION fc_descuento_por_cantidad(
+    p_cantidad INT,
+    p_precio_unitario DECIMAL(10,2)
+)
+RETURNS DECIMAL(10,2)
+DETERMINISTIC
+BEGIN
+    DECLARE descuento DECIMAL(10,2);
+
+    IF p_cantidad >= 5 THEN
+        SET descuento = p_precio_unitario * p_cantidad * 0.10;
+    ELSE
+        SET descuento = 0;
+    END IF;
+
+    RETURN descuento;
+END $$
+
+DELIMITER ;
+
+SELECT fc_descuento_por_cantidad(6, 12000) AS descuento_aplicado;
+
 
